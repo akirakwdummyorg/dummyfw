23442c38561f97d9835ff483cd97012763f68e4c
KAWAGUCHI, Akira <akirakw@gmail.com>
2011-03-27 14:14:21 +0900

    first commit.

---
+++ bar/src/main/java/dummyfw/App.java
@@ -0,0 +1,13 @@
+package dummyfw;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
